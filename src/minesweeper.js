2368deab887b19292fcc2baaa61bbad7f45fddaa
mfieldsboston <mfields11@gmail.com>
2017-10-13 00:23:31 -0400

    added code

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,20 +1,46 @@
 
 // begin 3rd evolution
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
-  const board = [];
-  //console.log('First Rows: ' + numberOfRows + ' First Columns: ' + numberOfColumns);
-  for (let i = 0; i < numberOfRows; i++) {
-      const row = [];
-      //console.log('Second Rows: ' + numberOfRows + ' Second Columns: ' + numberOfColumns + ' i: ' + i);
-    for (let j = 0; j < numberOfColumns; j++) {
-      //console.log('Third Rows: ' + numberOfRows  + ' Third Columns: ' + numberOfColumns + ' i: ' + i  + ' j: ' + j);
-      row.push(' ');
+    const board = [];
+    //console.log('First Rows: ' + numberOfRows + ' First Columns: ' + numberOfColumns);
+    for (let i = 0; i < numberOfRows; i++) {
+        const row = [];
+        //console.log('Second Rows: ' + numberOfRows + ' Second Columns: ' + numberOfColumns + ' i: ' + i);
+      for (let j = 0; j < numberOfColumns; j++) {
+        //console.log('Third Rows: ' + numberOfRows  + ' Third Columns: ' + numberOfColumns + ' i: ' + i  + ' j: ' + j);
+        row.push(' ');
+      }
+      board.push(row);
     }
-    board.push(row);
-  }
-  return board;
+    return board;
 };
 
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    const board = [];
+    //console.log('First Rows: ' + numberOfRows + ' First Columns: ' + numberOfColumns);
+    for (let i = 0; i < numberOfRows; i++) {
+        const row = [];
+        //console.log('Second Rows: ' + numberOfRows + ' Second Columns: ' + numberOfColumns + ' i: ' + i);
+      for (let j = 0; j < numberOfColumns; j++) {
+        //console.log('Third Rows: ' + numberOfRows  + ' Third Columns: ' + numberOfColumns + ' i: ' + i  + ' j: ' + j);
+        row.push(null);
+      }
+      board.push(row);
+    }
+
+    let numberOfBombsPlaced = 0;
+
+    while (numberOfBombsPlaced < numberOfBombs) {
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+        //An important note: The code in your while loop has the potential to place bombs on top of already existing bombs.
+        //This will be fixed when you learn about control flow
+    }
+
+    return board;
+};
 // end 3rd evolution
 
 
