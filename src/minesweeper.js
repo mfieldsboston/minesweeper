851be03a83647ec56b9915d8c5ebf04f8be5131b
mfieldsboston <mfields11@gmail.com>
2017-10-14 19:46:50 -0400

    updated version start

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,4 +1,63 @@
 
+// begin 4th evolution
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    const board = [];
+    //console.log('First Rows: ' + numberOfRows + ' First Columns: ' + numberOfColumns);
+    for (let i = 0; i < numberOfRows; i++) {
+        const row = [];
+        //console.log('Second Rows: ' + numberOfRows + ' Second Columns: ' + numberOfColumns + ' i: ' + i);
+      for (let j = 0; j < numberOfColumns; j++) {
+        //console.log('Third Rows: ' + numberOfRows  + ' Third Columns: ' + numberOfColumns + ' i: ' + i  + ' j: ' + j);
+        row.push(' ');
+      }
+      board.push(row);
+    }
+    return board;
+};
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    const board = [];
+    //console.log('First Rows: ' + numberOfRows + ' First Columns: ' + numberOfColumns);
+    for (let i = 0; i < numberOfRows; i++) {
+        const row = [];
+        //console.log('Second Rows: ' + numberOfRows + ' Second Columns: ' + numberOfColumns + ' i: ' + i);
+      for (let j = 0; j < numberOfColumns; j++) {
+        //console.log('Third Rows: ' + numberOfRows  + ' Third Columns: ' + numberOfColumns + ' i: ' + i  + ' j: ' + j);
+        row.push(null);
+      }
+      board.push(row);
+    }
+
+    let numberOfBombsPlaced = 0;
+
+    while (numberOfBombsPlaced < numberOfBombs) {
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+        //An important note: The code in your while loop has the potential to place bombs on top of already existing bombs.
+        //This will be fixed when you learn about control flow
+    }
+
+    return board;
+};
+
+const printBoard = board => {
+    onsole.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
+
+// end 4th evolution
+
+
+/*
 // begin 3rd evolution
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     const board = [];
@@ -43,7 +102,7 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 };
 
 const printBoard = board => {
-  console.log(board.map(row => row.join(' | ')).join('\n'));
+    onsole.log(board.map(row => row.join(' | ')).join('\n'));
 };
 
 let playerBoard = generatePlayerBoard(3, 4);
@@ -55,7 +114,7 @@ console.log('Bomb Board: ');
 printBoard(bombBoard);
 
 // end 3rd evolution
-
+*/
 
 /*
 // begin 2nd evolution
